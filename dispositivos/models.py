85adbf92ad13d12dd057d92db309c6f2c5bbabca
carlotee <carlosvega3031@gmail.com>
2025-08-28 20:03:49 -0400

    Actualizo modelo de Zona

--- dispositivos/models.py
+++ dispositivos/models.py
@@ -7,3 +7,11 @@ class Dispositivo(models.Model):
     
 def __str__(self):
     return self.nombre
+
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    ubicacion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
