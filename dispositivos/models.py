8648d627d121423d484b29edd66473c2121c8846
Carlos Vega <carlosvega3031@gmail.com>
2025-08-28 20:43:46 -0400

    Update models.py

--- dispositivos/models.py
+++ dispositivos/models.py
@@ -15,4 +15,5 @@ def __str__(self):
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-
+def __str__(self):
+    return self.nombre
