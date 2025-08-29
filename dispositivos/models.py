51ba71815972e6af7a1e052e93288b4710cf278e
carlotee <carlosvega3031@gmail.com>
2025-08-28 20:06:15 -0400

    Actualizo modelo de Categoria

--- dispositivos/models.py
+++ dispositivos/models.py
@@ -7,3 +7,10 @@ class Dispositivo(models.Model):
     
 def __str__(self):
     return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
