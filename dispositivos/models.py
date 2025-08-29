eeae7a3548ffe9638164126f8050bb3d61a36673
carlotee <carlosvega3031@gmail.com>
2025-08-28 20:18:03 -0400

    Actualizo modelo de dispositivo

--- dispositivos/models.py
+++ dispositivos/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class Dispositivo(models.Model):
     nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
     consumo = models.IntegerField()
     estado = models.BooleanField(default=True)
-    
-def __str__(self):
-    return self.nombre
+
+    def __str__(self):
+        return self.nombre
